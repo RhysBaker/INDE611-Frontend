d8b4c8825b5e73ea1de18e31131d8680e8d7ab5e
Rhys Baker <rhys.baker99@gmail.com>
2020-05-08 15:36:05 +0100

    MUI theme, axios,  Sketch cards added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 //components
 import Navbar from "./components/Navbar";
@@ -10,20 +12,39 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#8982ff",
+      main: "#6c63ff",
+      dark: "#4b45b2",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#83ffe1",
+      main: "#64ffda",
+      dark: "#46b298",
+      contrastText: "#fff",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
 
+import Sketch from "../components/Sketch.js";
 export class home extends Component {
+  state = {
+    sketches: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get("/sketches")
+      .then((res) => {
+        this.setState({
+          sketches: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
   render() {
+    let recentSketchesMarkUp = this.state.sketches ? (
+      this.state.sketches.map((sketch) => <Sketch sketch={sketch} />)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentSketchesMarkUp}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile....</p>
+        </Grid>
+      </Grid>
     );
   }
 }
